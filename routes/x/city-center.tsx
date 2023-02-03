80342029d112a6b178d14247430f751e316815bf
Jordan Reger <mail@jordanreger.com>
2023-02-03 14:35:47 -0500

    refactor: debugging

--- routes/x/city-center.tsx
+++ routes/x/city-center.tsx
@@ -30,14 +30,16 @@ export const handler: Handlers = {
   async GET(req, ctx) {
     const origin = new URL(req.url).origin;
     const get_streets_list = await fetch(`${origin}/streets?district=city-center`).then(res => res.json());
+    console.log(get_streets_list);
+    /*
     const streets: preact.JSX.Element[] = [];
     get_streets_list.forEach((street: string) => {
       streets.push(<li>// <a href={`/x/city-center/${street}`}>{street.replaceAll("-", " ").toLocaleUpperCase()}</a></li>);
     })
 
     const streets_list = <ul>{streets}</ul>;
-
-    return ctx.render(streets_list);
+    */
+    return ctx.render("test");
   }
 }
 
