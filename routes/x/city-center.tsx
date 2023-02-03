d8e2888477de37cdfcc91f0cbaac127ba9154146
Jordan Reger <mail@jordanreger.com>
2023-02-03 14:42:37 -0500

    debug: i think it was accidentally reading the string as a comment

--- routes/x/city-center.tsx
+++ routes/x/city-center.tsx
@@ -29,16 +29,14 @@ const cityscape = `
 export const handler: Handlers = {
   async GET(req, ctx) {
     const origin = new URL(req.url).origin;
-    //const get_streets_list = await fetch(`${origin}/streets?district=city-center`).then(res => res.json());
-    /*
+    const get_streets_list = await fetch(`${origin}/streets?district=city-center`).then(res => res.json());
     const streets: preact.JSX.Element[] = [];
     get_streets_list.forEach((street: string) => {
-      streets.push(<li>// <a href={`/x/city-center/${street}`}>{street.replaceAll("-", " ").toLocaleUpperCase()}</a></li>);
+      streets.push(<li>{"//"} <a href={`/x/city-center/${street}`}>{street.replaceAll("-", " ").toLocaleUpperCase()}</a></li>);
     })
 
     const streets_list = <ul>{streets}</ul>;
-    */
-    return ctx.render("test");
+    return ctx.render(streets_list);
   }
 }
 
